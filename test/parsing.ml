3f48e0e76d85845dcac8b60703addee7261fc598
Mike McClurg <mike.mcclurg@gmail.com>
2013-05-15 19:02:39 +0100

    untabify parsing.ml
    Signed-off-by: Mike McClurg <mike.mcclurg@gmail.com>

--- test/parsing.ml
+++ test/parsing.ml
@@ -1,15 +1,15 @@
 (* Test cases to be parsed by ofactor *)
 
 let f a b =
-	let g c d =
-		match c with
-		| true -> a + d
-		| false -> b + d in
-	let rec h e f =
-		e + f + b in
-	g true (h a b)
+  let g c d =
+    match c with
+    | true -> a + d
+    | false -> b + d in
+  let rec h e f =
+    e + f + b in
+  g true (h a b)
 
 let string_of_pos (pos : Lexing.position) =
-	Printf.sprintf "l%dc%d"
-		pos.Lexing.pos_lnum
-		(pos.Lexing.pos_cnum - pos.Lexing.pos_bol)
+  Printf.sprintf "l%dc%d"
+    pos.Lexing.pos_lnum
+    (pos.Lexing.pos_cnum - pos.Lexing.pos_bol)
